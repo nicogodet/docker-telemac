53d95a6286ae118473abd0c55d05521eeac6e207
Nicolas Godet <nicolas.godet@outlook.fr>
2021-08-07 16:48:29 +0200

    Update V8 image

--- docker/cleanup.sh
+++ docker/cleanup.sh
@@ -1,4 +1,7 @@
+#!/bin/bash -ev
+
 echo "Remove unused files (examples, documentation, notebooks,...)"
+source ${TELEMAC_ROOT}/setenv.sh
 rm -rfv ${HOMETEL}/documentation
 rm -rfv ${HOMETEL}/examples
 rm -rfv ${HOMETEL}/notebooks
